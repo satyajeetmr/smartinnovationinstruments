50b4a5ac8c8c95501b4d22c677fb94ffc3aba32a
user <satyajeet.maurya@kelltontech.com>
2018-12-03 22:36:32 +0530

    03-12-2018 commit: before start work commit

--- header.php
+++ header.php
@@ -14,7 +14,16 @@
         <meta charset="<?php bloginfo('charset'); ?>">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/images/favicon-icon.png" type="image/favicon-icon.icon" />
-        <link rel="profile" href="http://gmpg.org/xfn/11">       
+        <link rel="profile" href="http://gmpg.org/xfn/11">      
+        
+            <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-TFDT6T8');</script>
+    <!-- End Google Tag Manager -->
+
         <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
         <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
         <!--[if lt IE 9]>
@@ -22,9 +31,32 @@
           <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
         <![endif]-->
         <?php wp_head(); ?>
+        <!-- Global Site Tag (gtag.js) - Google Analytics -->
+<!--        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-129344065-1"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'UA-129344065-1');
+        </script>-->
+        <script>
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+          ga('create', 'UA-129344065-1', 'auto');
+          ga('send', 'pageview');
+        </script>
+        <meta name="google-site-verification" content="2mDH5Ngfvt_0M514MuYTwufXecXh92S_ah7T-FC52w8" />
     </head>
 
     <body <?php body_class(); ?>>
+        <!-- Google Tag Manager (noscript) -->
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TFDT6T8"
+        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+        <!-- End Google Tag Manager (noscript) -->
+
         <div id="page">
             <header>		
                 <nav id="Mobile_menu">
